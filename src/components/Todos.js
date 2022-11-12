3ed10e8c40ff2e11dd7fa812633728f71f643528
devaaditya28 <devaaditya28@gmail.com>
2022-11-12 11:22:21 +0700

    add styling & animation

--- src/components/Todos.js
+++ src/components/Todos.js
@@ -1,50 +1,59 @@
-import React, { useRef, useState } from "react";
+import React, { useState } from "react";
 import { connect } from "react-redux";
-import { addTodos, completeTodos, removeTodos, updateTodos } from "../redux/reducer";
+import { addTodos } from "../redux/reducer";
+import { GoPlus } from "react-icons/go";
+import { motion } from "framer-motion";
 
 const mapStateToProps = (state) => {
     return {
-        todos: state,
-    }
-}
-
+      todos: state,
+    };
+};
+  
 const mapDispatchToProps = (dispatch) => {
     return {
-        addTodo: (obj) => dispatch(addTodos(obj)),
-        removeTodo: (id) => dispatch(removeTodos(id)),
-        updateTodo: (obj) => dispatch(updateTodos(obj)),
-        completeTodo: (id) => dispatch(completeTodos(id)),
-    }
-}
-
+      addTodo: (obj) => dispatch(addTodos(obj)),
+    };
+};
+  
 const Todos = (props) => {
     const [todo, setTodo] = useState("");
-
+  
     const handleChange = (e) => {
-        setTodo(e.target.value);
+      setTodo(e.target.value);
+    };
+
+    const add = () => {
+        if (todo === "") {
+          alert("Input is Empty");
+        } else {
+          props.addTodo({
+            id: Math.floor(Math.random() * 1000),
+            item: todo,
+            completed: false,
+          });
+          setTodo("");
+        }
     };
 
     // console.log("props from store", props);
 
     return (
         <div className="addTodos">
-            <input 
-                type="text" 
-                onChange={(e) => handleChange(e)} 
-                className="todo-input" 
+            <input
+                type="text"
+                onChange={(e) => handleChange(e)}
+                className="todo-input"
+                value={todo}
             />
-            <button 
-                className="add-btn" 
-                onClick={() => 
-                    props.addTodo({
-                        id: Math.floor(Math.random() * 1000),
-                        item: todo,
-                        completed: false,
-                    })
-                }
-            > 
-                Add 
-            </button>
+            <motion.button
+                whileHover={{ scale: 1.1 }}
+                whileTap={{ scale: 0.9 }}
+                className="add-btn"
+                onClick={() => add()}
+            >
+                <GoPlus />
+            </motion.button>
             <br />
         </div>
     )
--- src/redux/reducer.js
+++ src/redux/reducer.js
@@ -18,34 +18,34 @@ const addTodoReducer = createSlice({
         //update todos
         updateTodos: (state, action) => {
             return state.map((todo) => {
-                if (todo.id === action.payload.id) {
-                    return {
-                    ...todo,
-                    item: action.payload.item,
-                    };
-                }
-            return todo;
+              if (todo.id === action.payload.id) {
+                return {
+                  ...todo,
+                  item: action.payload.item,
+                };
+              }
+              return todo;
             });
         },
         // completed
         completeTodos: (state, action) => {
             return state.map((todo) => {
-                if (todo.id === action.payload) {
-                    return {
-                        ...todo,
-                        completed: true,
-                  };
-                }
-                return todo;
+              if (todo.id === action.payload) {
+                return {
+                  ...todo,
+                  completed: true,
+                };
+              }
+              return todo;
             });
-        }
+        },
     },
-})
+});
 
-export const { 
-    addTodos, 
-    removeTodos, 
-    updateTodos, 
-    completeTodos 
+export const {
+    addTodos,
+    removeTodos,
+    updateTodos,
+    completeTodos,
 } = addTodoReducer.actions;
 export const reducer = addTodoReducer.reducer;
